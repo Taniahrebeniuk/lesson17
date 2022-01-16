5152b0e1c1fff7bd1b431414297c773863b347e9
tetianahrebeniuk <taniahrebeniuk@gmail.com>
2022-01-16 19:11:37 +0200

    update

--- src/test/java/org/ex/BaseClass.java
+++ src/test/java/org/ex/BaseClass.java
@@ -1,5 +1,6 @@
 package org.ex;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
@@ -10,14 +11,15 @@ public class BaseClass {
 
     WebDriver driver;
 
+
     @BeforeClass
     public void setupApplication()
     {
 
         Reporter.log("=====Browser Session Started=====", true);
 
-        driver=new ChromeDriver();
-
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get("http://enterprise.demo.orangehrmlive.com/symfony/web/index.php/auth/login");
